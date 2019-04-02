72361e649f994e1223c851dc4d8e0276faee0ed0
Michael Kohl <citizen428@gmail.com>
2019-04-03 00:11:18 +0700

    Chapter 4

--- ray.ml
+++ ray.ml
@@ -2,9 +2,21 @@ open Vec3
 
 type ray = { origin: vec3; direction: vec3 }
 
+(* Check if ray hits a sphere defined by center and radius *)
+let hit_sphere center radius ray =
+  let oc = ray.origin -: center in
+  let a = dot ray.direction ray.direction in
+  let b = 2. *. dot oc ray.direction in
+  let c = dot oc oc -. (radius *. radius) in
+  let discrimant = b *. b -. 4. *. a *. c in
+  discrimant > 0.
+
 (* linear blend of white and blue *)
 let color r =
-  let unit_direction = unit_vector r.direction in
-  let t = 0.5 *. (unit_direction.y +. 1.) in
-  ({ x = 1.; y = 1.; z = 1. } *@ (1.0 -. t)) +:
-  ({ x = 0.5; y = 0.7; z = 1. } *@ t)
+  if hit_sphere { x = 0.; y = 0.; z = -1. } 0.5 r
+  then { x = 1.; y = 0.; z = 0. }
+  else
+    let unit_direction = unit_vector r.direction in
+    let t = 0.5 *. (unit_direction.y +. 1.) in
+    ({ x = 1.; y = 1.; z = 1. } *@ (1.0 -. t)) +:
+    ({ x = 0.5; y = 0.7; z = 1. } *@ t)
--- vec3.ml
+++ vec3.ml
@@ -5,12 +5,18 @@ type vec3 = { x: float; y: float; z: float }
 (* Vector addition *)
 let (+:) v1 v2 = { x = v1.x +. v2.x; y = v1.y +. v2.y; z = v1.z +. v2.z }
 
+(* Vector subtraction *)
+let (-:) v1 v2 = { x = v1.x -. v2.x; y = v1.y -. v2.y; z = v1.z -. v2.z }
+
 (* Multiply vector with scalar *)
 let ( *@ ) v t = { x = v.x *. t ; y = v.y *. t ; z = v.z *. t }
 
 (* Divide vector by scalar *)
-let ( /@ ) v t = { x = v.x /. t ; y = v.y /. t ; z = v.z /. t }
+let (/@) v t = { x = v.x /. t ; y = v.y /. t ; z = v.z /. t }
 
 let length v = sqrt (v.x *. v.x +. v.y *. v.y +. v.z *. v.z)
 
 let unit_vector v = v /@ length v
+
+(* Dot product *)
+let dot v1 v2 = v1.x *. v2.x +. v1.y *. v2.y +. v1.z *. v2.z
